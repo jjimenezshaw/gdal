6fdda35a4321d345d675da44e8b5d0034197b740
Javier Jimenez Shaw <j1@jimenezshaw.com>
2024-07-30 15:37:42 +0200

    support complex interpolateatpoint

--- alg/gdal_interpolateatpoint.h
+++ alg/gdal_interpolateatpoint.h
@@ -54,7 +54,8 @@ bool GDALInterpolateAtPoint(GDALRasterBand *pBand,
                             GDALRIOResampleAlg eResampleAlg,
                             std::unique_ptr<DoublePointsCache> &cache,
                             const double dfXIn, const double dfYIn,
-                            double *pdfOutputValue);
+                            double *pdfOutputReal,
+                            double *pdfOutputImag);
 
 /*! @endcond */
 
